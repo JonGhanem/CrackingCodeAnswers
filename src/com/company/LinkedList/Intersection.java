c7e1df76259c571d3658df29954bbbf62af30f7f
Yahya Ghanem <yahya.ghanem@nweave.com>
2021-10-02 19:38:02 +0200

    Work Done: ========= - Solve intersection

--- src/com/company/LinkedList/Intersection.java
+++ src/com/company/LinkedList/Intersection.java
@@ -1,6 +1,7 @@
 package com.company.LinkedList;
 
 import java.util.HashSet;
+import java.util.List;
 
 public class Intersection {
 
@@ -26,6 +27,28 @@ public class Intersection {
             }
         }
         return null;
+    }
+
+    public LinkedListNode getIntersectedNode(LinkedListNode n1, LinkedListNode n2){
+        if(n1 == null || n2 == null) return null;
+
+        LinkedListNode a_pointer = n1;
+        LinkedListNode b_pointer = n2;
+
+        while(a_pointer != b_pointer){
+            if(a_pointer == null){
+                a_pointer = n2;
+            } else {
+                a_pointer = a_pointer.next;
+            }
+
+            if(b_pointer == null){
+                b_pointer = n1;
+            } else {
+                b_pointer = b_pointer.next;
+            }
+        }
+        return a_pointer;
     }
 
 
