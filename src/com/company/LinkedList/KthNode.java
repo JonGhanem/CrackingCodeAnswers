b477d655f8f89c0c117383fb42ac501ab2733eed
Yahya Ghanem <yahya.ghanem@nweave.com>
2021-10-02 20:55:06 +0200

    Work Done: ========= - Solve remove n node from the last

--- src/com/company/LinkedList/KthNode.java
+++ src/com/company/LinkedList/KthNode.java
@@ -2,6 +2,28 @@ package com.company.LinkedList;
 
 public class KthNode {
 
+
+    public LinkedListNode removeNthFromEnd(LinkedListNode head, int n) {
+
+        LinkedListNode start = new LinkedListNode(0);
+        LinkedListNode slow = start, fast = start;
+        slow.next = head;
+
+        //Move fast in front so that the gap between slow and fast becomes n
+        for(int i=1; i<=n+1; i++)   {
+            fast = fast.next;
+        }
+        //Move fast to the end, maintaining the gap
+        while(fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        //Skip the desired node
+        slow.next = slow.next.next;
+        return start.next;
+    }
+
+
     public LinkedListNode getNode(LinkedListNode head, int index){
         LinkedListNode fast = head;
         LinkedListNode slow = head;
